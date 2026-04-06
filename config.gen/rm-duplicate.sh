38a67c5d48f78e0174231ac6db8b71a13579e031
ferry133 <ferry133@gmail.com>
2026-04-06 16:53:28 +0800

    4

--- config.gen/rm-duplicate.sh
+++ config.gen/rm-duplicate.sh
@@ -7,22 +7,6 @@
 #    and, temporarily disable:
 #   C) cilium release inside ./bootstrap/helmfile.d/01-apps.yaml
 #   D) ./envoy-gateway/ks.yaml & ./k8s-gateway/ks.yaml inside ./kubernetes/apps/network/kustomization.yaml
-
-# 3) execute "task bootstrap:apps". Might need to re-execute several times until all the resources are applied successfully.
-# 4) Verify the cloudflare-tunnel, cloudflare-tunnel-lan and coredns are working properly. The tunnel status should be "healthy" which indicates outbound connectivity to cloudflare network successfully.
-
-# 5) After the cluster is up and running, now we can:
-#   A) resume 2.C), 2.D) setting
-#   B) kubectl delete all the flannel, kube-proxy resources in kube-system, including deployment, service, ServiceAccount and configmap.
-#   C) then execute "task bootstrap:apps" again to apply the cilium and envoy gateway resources.
-
-
-
-# 2) before "task bootstrap:apps", need to kubectl delete all the coredns/flannel resources in kube-system,
-#    including deployment, service, ServiceAccount and configmap.
-# 3) search service cidr "10.43" which should be changed to 10.96.
-#helm uninstall coredns -n kube-system
-
 #
 # 2.B) commmands:
 #
@@ -31,6 +15,16 @@ kubectl delete service kube-dns -n kube-system
 kubectl delete serviceaccount coredns -n kube-system
 kubectl delete configmap coredns -n kube-system
 
+
+
+
+# 3) execute "task bootstrap:apps". Might need to re-execute several times until all the resources are applied successfully.
+# 4) Verify the cloudflare-tunnel, cloudflare-tunnel-lan and coredns are working properly. The tunnel status should be "healthy" which indicates outbound connectivity to cloudflare network successfully.
+
+# 5) After the cluster is up and running, now we can:
+#   A) resume 2.C), 2.D) setting
+#   B) kubectl delete all the flannel, kube-proxy resources in kube-system, including deployment, service, ServiceAccount and configmap.
+#   C) then execute "task bootstrap:apps" again to apply the cilium and envoy gateway resources.
 #
 # 5.B) commmands:
 #
@@ -43,6 +37,17 @@ kubectl delete ClusterRole/flannel
 kubectl delete daemonset kube-proxy -n kube-system
 kubectl delete configmap kube-proxy -n kube-system
 kubectl delete serviceaccount kube-proxy -n kube-system
+
+
+
+
+
+
+
+# 2) before "task bootstrap:apps", need to kubectl delete all the coredns/flannel resources in kube-system,
+#    including deployment, service, ServiceAccount and configmap.
+# 3) search service cidr "10.43" which should be changed to 10.96.
+#helm uninstall coredns -n kube-system
 
 
 
