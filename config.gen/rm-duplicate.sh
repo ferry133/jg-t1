48be60d1a9dc748def5b66392262c898b39c5d54
ferry133 <ferry133@gmail.com>
2026-04-06 13:41:25 +0800

    1

--- config.gen/rm-duplicate.sh
+++ config.gen/rm-duplicate.sh
@@ -3,7 +3,7 @@
 # 1) no need to execute: "task bootstrap:talos"
 # 2) before "task bootstrap:apps", need to kubectl delete all the coredns/flannel resources in kube-system,
 #    including deployment, service, ServiceAccount and configmap.
-#
+# 3) search service cidr "10.43" which should be changed to 10.96.
 helm uninstall coredns -n kube-system
 
 kubectl delete deployment coredns -n kube-system
